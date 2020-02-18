1c63f2e82146e28ce2346299be9b8036023352e6
Sourav <songokuonfire@gmail.com>
2020-02-18 08:37:16 +0530

    node

---
+++ notes-app/app.js
@@ -0,0 +1,12 @@
+// const add = require('./utils.js');
+
+// const sum  = add(4,2);
+
+// console.log(sum);
+
+
+const getNotes = require('./notes.js');
+
+const ans = getNotes();
+
+console.log(ans);
